a0b20934283893c4e7aee678be8c639b22da0132
Lennart Buhl <git@hackmate.de>
2015-06-03 21:16:14 +0200

    started work on Prolog trans

---
+++ prolog/trans.pl
@@ -0,0 +1,16 @@
+%%%%%%%%%%%%%%%%%%% AM Operations
+%   read(Int), write(Int),
+%   load(Int), store(Int), lit(BracketlessInt),
+%   add, mul, sub, div, mod,
+%   lt, eq, ne, gt, le, ge,
+%   jmp(Counter), jmc(Counter),
+%   nop
+
+% opAddSub: addOP, subOP können direkt übernommen werden aus AST
+% opMulDivMod: mulOP, divOP, modOP können direkt übernommen werden aus AST
+% relation: lt, eq, ne, gt, le, ge können direkt übernommen werden aus AST
+
+
+
+
+
